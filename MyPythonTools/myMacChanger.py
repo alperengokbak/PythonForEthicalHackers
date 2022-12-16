9da6bd42eb30feb2da444c3685372a1365f3781d
StrqSS <alperengokbak@gmail.com>
2022-12-17 01:35:06 +0300

    Update python3 version

--- MyPythonTools/myMacChanger.py
+++ MyPythonTools/myMacChanger.py
@@ -2,32 +2,40 @@ import subprocess as sub
 import optparse
 import re
 
+
 def getInput():
     parseObject = optparse.OptionParser()
-    parseObject.add_option("-i", "--interface", dest="interface", help="Interface to change!")
-    parseObject.add_option("-m", "--mac", dest="macAddress", help="New MAC Address")
+
+    parseObject.add_option("-i", "--interface",
+                           dest="interface", help="Interface to change!")
+
+    parseObject.add_option(
+        "-m", "--mac", dest="macAddress", help="New MAC Address")
 
     return parseObject.parse_args()
 
+
 def changeAddress(userInterface, userMacAddress):
-    sub.call(["ifconfig",userInterface,"down"])
-    sub.call(["ifconfig",userInterface,"hw","ether",userMacAddress])
-    sub.call(["ifconfig",userInterface,"up"])
+    sub.call(["ifconfig", userInterface, "down"])
+    sub.call(["ifconfig", userInterface, "hw", "ether", userMacAddress])
+    sub.call(["ifconfig", userInterface, "up"])
+
 
 def controlMac(interface):
-    ifconfig = sub.check_output(["ifconfig",interface])
-    newMac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig)
+    ifconfig = sub.check_output(["ifconfig", interface])
+    newMac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig))
 
     if newMac:
         return newMac.group(0)
     else:
         return None
 
+
 print("Mac Changer started...")
 
 (userInput, arguments) = getInput()
-changeAddress(userInput.interface,userInput.macAddress)
-finalizedMac = controlMac(userInput.interface)
+changeAddress(userInput.interface, userInput.macAddress)
+finalizedMac = controlMac(str(userInput.interface))
 if finalizedMac == userInput.macAddress:
     print("You changed MAC Address successfully...")
 else:
