02d8d814dd46bb2927d52597dd8528d0d14ab67f
StrqSS <alperengokbak@gmail.com>
2022-12-17 01:13:47 +0300

    Added Pyhton Tools and README.md

---
+++ MyPythonTools/README.md
@@ -0,0 +1,2 @@
+#### I shared the whloe pyhton course in before time. Now, I would like to share with some codeblocks about special pyhton libraries. And made by these tools. First of them;
+1) MAC Address changer(MyMacChanger)
---
+++ MyPythonTools/myMacChanger.py
@@ -0,0 +1,34 @@
+import subprocess as sub
+import optparse
+import re
+
+def getInput():
+    parseObject = optparse.OptionParser()
+    parseObject.add_option("-i", "--interface", dest="interface", help="Interface to change!")
+    parseObject.add_option("-m", "--mac", dest="macAddress", help="New MAC Address")
+
+    return parseObject.parse_args()
+
+def changeAddress(userInterface, userMacAddress):
+    sub.call(["ifconfig",userInterface,"down"])
+    sub.call(["ifconfig",userInterface,"hw","ether",userMacAddress])
+    sub.call(["ifconfig",userInterface,"up"])
+
+def controlMac(interface):
+    ifconfig = sub.check_output(["ifconfig",interface])
+    newMac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig)
+
+    if newMac:
+        return newMac.group(0)
+    else:
+        return None
+
+print("Mac Changer started...")
+
+(userInput, arguments) = getInput()
+changeAddress(userInput.interface,userInput.macAddress)
+finalizedMac = controlMac(userInput.interface)
+if finalizedMac == userInput.macAddress:
+    print("You changed MAC Address successfully...")
+else:
+    print("Error!")
